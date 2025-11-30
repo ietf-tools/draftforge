2463d5f29e40412ae994f86ec103a6a3a18a66f8
NGPixel <github@ngpixel.com>
2025-11-30 04:02:49 -0500

    feat: add "Add XML Models" tool to download XML schemas to document folder

--- views/tools.js
+++ views/tools.js
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode'
+import { posix } from 'node:path'
 
 class ToolsProvider {
   constructor() {
@@ -11,6 +12,7 @@ class ToolsProvider {
       { id: 'formatDocument', label: 'Format Document', description: 'Reformat document and fix indentation', icon: 'list-flat' },
       { id: 'openPreview', label: 'Open Preview', description: 'Open a preview for the current draft', icon: 'open-preview' },
       { id: 'exportPdf', label: 'Export as PDF', description: 'Export the current draft to PDF', icon: 'file-pdf' },
+      { id: 'addXmlModels', label: 'Add XML Models', description: 'Download and add RelaxNG schema files to the document directory', icon: 'cloud-download' },
       { id: 'stripMLineEndings', label: 'Strip ^M Line Endings', description: 'Clean Document from ^M Line Endings', icon: 'no-newline' }
     ]
   }
@@ -98,6 +100,49 @@ export function activateToolsView (context) {
           break
         case 'exportPdf':
           vscode.window.showInformationMessage('Export to PDF not implemented yet.')
+          break
+        case 'addXmlModels':
+          const editor = vscode.window.activeTextEditor
+          if (!editor) {
+            return vscode.window.showInformationMessage('Open an XML document first.')
+          }
+
+          // Fetch schemas
+
+          try {
+            const dirPath = posix.dirname(editor.document.uri.toString())
+            const rfc7991bisContent = await fetch('https://github.com/ietf-tools/RFCXML/raw/main/rfc7991bis.rnc')
+            await vscode.workspace.fs.writeFile(vscode.Uri.parse(posix.join(dirPath, 'rfc7991bis.rnc')), new Uint8Array(await rfc7991bisContent.arrayBuffer()))
+            const svg12rfcContent = await fetch('https://github.com/ietf-tools/RFCXML/raw/main/SVG-1.2-RFC.rnc')
+            await vscode.workspace.fs.writeFile(vscode.Uri.parse(posix.join(dirPath, 'SVG-1.2-RFC.rnc')), new Uint8Array(await svg12rfcContent.arrayBuffer()))
+          } catch (err) {
+            return vscode.window.showErrorMessage(`Failed to download schemas to disk: ${err.message}`)
+          }
+
+          // Add reference to XML document
+
+          let replaceOccured = false
+          for (let i = 0; i < doc.lineCount; i++) {
+            const line = doc.lineAt(i)
+            if (line.text.startsWith('<?xml-model')) {
+              await editor.edit(editBuilder => {
+                editBuilder.replace(line.range, '<?xml-model href="rfc7991bis.rnc"?>')
+              })
+              replaceOccured = true
+            }
+          }
+
+          if (replaceOccured) {
+            vscode.window.showInformationMessage('Schemas downloaded and existing <?xml-model> updated successfully.')
+          } else {
+            await editor.edit(editBuilder => {
+              editBuilder.insert(
+                new vscode.Position(doc.lineAt(0).text.startsWith('<?xml ') ? 1 : 0, 0),
+                '<?xml-model href="rfc7991bis.rnc"?>\n'
+              )
+            })
+            vscode.window.showInformationMessage('Schemas downloaded and <?xml-model> added successfully.')
+          }
           break
         case 'stripMLineEndings': {
           const editor = vscode.window.activeTextEditor
