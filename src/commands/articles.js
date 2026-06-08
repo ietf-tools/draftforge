7aa9f33f314336fe514648517d51e3ed2861bd40
NGPixel <github@ngpixel.com>
2026-06-07 21:55:17 -0400

    fix: support quotes in articles check

--- src/commands/articles.js
+++ src/commands/articles.js
@@ -28,14 +28,14 @@ export function registerCheckArticlesCommand(context, diagnosticCollection, igno
         const eligibleIgnores = ignores[activeDoc.uri.toString()]?.articles ?? []
 
         const partARgx =
-          /(?<!(?:[aA]ppendix|[cC]onnection|[lL]ink|[nN]ode|Operator)) (?!(?:[aA] (?:AAA|Europe|[oO]ne|U[A-Z]|U-label|[uU]biquitous|[uU]nicast|[uU]nicode|[uU]nidir|[uU]nif|[uU]nion|[uU]nique|[uU]nit|[uU]nivers|[uU]sable|[uU]sability|[uU]sage|[uU]se|[uU]tility)|a uCDN|A and))[aA] [aeiouAEIOU]/g
+          /(?<!(?:[aA]ppendix|[cC]onnection|[lL]ink|[nN]ode|Operator)) (?!(?:[aA] "?(?:AAA|Europe|[oO]ne|U[A-Z]|U-label|[uU]biquitous|[uU]nicast|[uU]nicode|[uU]nidir|[uU]nif|[uU]nion|[uU]nique|[uU]nit|[uU]nivers|[uU]sable|[uU]sability|[uU]sage|[uU]se|[uU]tility)|a uCDN|A and))[aA] "?[aeiouAEIOU]/g
         const partARRgx =
-          /(?!(?:[aA] (?:RADIUS|RECEIVE|RECOMMENDED|REFER|RELOAD|RST|REALM|RESERVATION|REQUEST|RESET|ROUTE|RPL)))[aA] R[A-Z]/g
+          /(?!(?:[aA] "?(?:RADIUS|RECEIVE|RECOMMENDED|REFER|RELOAD|RST|REALM|RESERVATION|REQUEST|RESET|ROUTE|RPL)))[aA] "?R[A-Z]/g
         const partBRgx =
-          / (?!(?:[aA]n (?:hour|honest|honor|Mtrace|x-coordinate|x coordinate|A[A-Z]|E[A-Z]|F[A-Z]|H[A-Z]|I[A-Z]|L[A-Z]|L[0-9][A-Z]|M[A-Z]|N[A-Z]|O[A-Z]|R[A-Z]|R[0-9]|S[A-Z]|X[A-Z]|X\.509|xTR)))[aA]n [b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]/g
+          / (?!(?:[aA]n "?(?:hour|honest|honor|Mtrace|x-coordinate|x coordinate|A[A-Z]|E[A-Z]|F[A-Z]|H[A-Z]|I[A-Z]|L[A-Z]|L[0-9][A-Z]|M[A-Z]|N[A-Z]|O[A-Z]|R[A-Z]|R[0-9]|S[A-Z]|X[A-Z]|X\.509|xTR)))[aA]n "?[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]/g
         const partCRgx =
-          /[aA]n (?:AAA|FEC|FIR|LIS|LRDD|MEG|MEP|MRHOF|MIC|NAPTR|NAT|NAS|RAS|ROHC|RPL|RST|SAFI|SCSI|SID|SIP|SMPTE|SYN|rinit)/g
-        const partCLFRgx = /[aA]n LF /g
+          /[aA]n "?(?:AAA|FEC|FIR|LIS|LRDD|MEG|MEP|MRHOF|MIC|NAPTR|NAT|NAS|RAS|ROHC|RPL|RST|SAFI|SCSI|SID|SIP|SMPTE|SYN|rinit)/g
+        const partCLFRgx = /[aA]n "?LF"? /g
 
         const diags = []
         for (let lineIdx = 0; lineIdx < activeDoc.lineCount; lineIdx++) {
